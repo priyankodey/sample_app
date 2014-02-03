f79e3f4f530a5980a70d825055285ae3e2180ee0
priyanko <priynko.priyankodey87@gmail.com>
2014-02-03 18:53:12 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  root "static_pages#home"
+ 
+ resources :users
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
